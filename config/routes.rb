a38ea97ff9988cb2b4fe2685d7d7db48930bbaad
OTR500miles2go <32044213+OTR500miles2go@users.noreply.github.com>
2018-12-31 18:40:29 -0600

    Ability to Complete Tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :items 
+
+  resources :items do
+    patch :complete
+  end
+
   root 'items#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_31_145145) do
+ActiveRecord::Schema.define(version: 2018_12_31_184608) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_12_31_145145) do
     t.datetime "updated_at", null: false
     t.string "user_id"
     t.string "integer"
+    t.datetime "completed_at"
   end
 
   create_table "users", force: :cascade do |t|
